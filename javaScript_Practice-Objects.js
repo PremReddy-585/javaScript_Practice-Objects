b06bd71f251e6422d8842919e91ef5e946bb398e
Prem_Saragada <149216070+PremReddy-585@users.noreply.github.com>
2024-01-14 19:40:00 +0530

    Create  javaScript_Practice-Objects.js

---
+++ javaScript_Practice-Objects.js
@@ -0,0 +1,24 @@
+//create and log an object
+let vehicle = {
+    name: 'Mercedes Benz',
+    model: 'C-Class',
+    manufacturer: 'Daimler AG company',
+    length: '4686mm',
+    'cargo capacity': '12.6 cubic feet'
+};
+
+console.log(vehicle);
+
+//Create & Log an Object - 2
+let bahubali = {
+    title: "Baahubali",
+    director: "Rajamouli",
+    producers: ["Sobhu", "Raghavendra", "Prasad"],
+    "lead actors with their roles": {
+        prabhas: "Baahubali",
+        anushka: "Devasena",
+        rana: "Bhallaladeva",
+        tamannah: "Avanthika"
+    }
+};
+console.log(bahubali);
